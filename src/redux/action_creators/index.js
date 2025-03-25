93ccd21b28879a9f65f94e7ac31f3ddca9d1524a
stefany marinho <stefanymarinholg@gmail.com>
2025-03-24 21:08:47 -0300

    alteracao

--- src/redux/action_creators/index.js
+++ src/redux/action_creators/index.js
@@ -1,4 +1,4 @@
-import {
+import { // importar ações da pasta actions 
   GET_CHARACTERS,
   RESET_CHARACTERS,
   GET_CHARACTER,
@@ -13,27 +13,26 @@ import {
   REMOVE_CHARACTER_FROM_FAVOURITES,
   ADD_COMIC_TO_FAVOURITES,
   REMOVE_COMIC_FROM_FAVOURITES,
-  // GET_SERIES,
   GET_STORIES,
-  // GET_CREATORS,
 } from "../actions";
-import md5 from "js-md5";
+import md5 from "js-md5"; //importação da biblioteca md5 para geração de hash
 
-const PUBLIC_KEY = "895f2dd3d5a162aa45ef3e70be4bd69d";
+//  chaves da API 
+const PUBLIC_KEY = "895f2dd3d5a162aa45ef3e70be4bd69d"; 
 const TIME_STAMP = Date.now().toString();
-
+// funcao que gera o hash para autenticar API
 const hashGenerator = () => {
-  const PRIVATE_KEY = "2ee45e44c5fbae6693edb932e84171aed01f3f22"; //
+  const PRIVATE_KEY = "2ee45e44c5fbae6693edb932e84171aed01f3f22"; 
   const HASH = md5.create();
   HASH.update(TIME_STAMP + PRIVATE_KEY + PUBLIC_KEY);
   return HASH.hex();
 };
-
+// Ação para buscar personagens
 export const getCharacters = (character) => {
-  const hash = hashGenerator();
+  const hash = hashGenerator(); //gera hash
   const URL = `https://gateway.marvel.com:443/v1/public/characters?ts=${TIME_STAMP}&nameStartsWith=${character}&limit=50&apikey=${PUBLIC_KEY}&hash=${hash}`;
   return (dispatch) => {
-    fetch(URL)
+    fetch(URL) // faz a requisição à API
       .then((response) => response.json())
       .then((data) => {
         dispatch({ type: GET_CHARACTERS, payload: data.data.results });
@@ -42,14 +41,14 @@ export const getCharacters = (character) => {
       .catch((err) => console.error(err));
   };
 };
-
+// Ação para resetar a lista de personagens 
 export const resetCharacters = () => {
   console.log("reset");
   return {
     type: RESET_CHARACTERS,
   };
 };
-
+// Busca um personagem especifico pelo nome
 export const getCharacter = (name) => {
   const hash = hashGenerator();
   const URL = `https://gateway.marvel.com:443/v1/public/characters?name=${name}&ts=${TIME_STAMP}&apikey=${PUBLIC_KEY}&hash=${hash}`;
@@ -63,7 +62,7 @@ export const getCharacter = (name) => {
       .catch((err) => console.error(err));
   };
 };
-
+// Busca quadrinhos especificos com o ID
 export const getCharacterComics = (id) => {
   const hash = hashGenerator();
   const URL = `https://gateway.marvel.com:443/v1/public/characters/${id}/comics?ts=${TIME_STAMP}&limit=50&apikey=${PUBLIC_KEY}&hash=${hash}`;
@@ -77,7 +76,7 @@ export const getCharacterComics = (id) => {
       .catch((err) => console.error(err));
   };
 };
-
+// busca eventos especificos pelo id 
 export const getCharacterEvents = (id) => {
   const hash = hashGenerator();
   const URL = `https://gateway.marvel.com:443/v1/public/characters/${id}/events?ts=${TIME_STAMP}&apikey=${PUBLIC_KEY}&hash=${hash}`;
@@ -91,19 +90,19 @@ export const getCharacterEvents = (id) => {
       .catch((err) => console.error(err));
   };
 };
-
+// reseta a lista de quadrinhos de um personagem
 export const resetCharacterComics = () => {
   return {
     type: RESET_CHARACTER_COMICS,
   };
 };
-
+// reseta a lista de eventos de um personagem 
 export const resetCharacterEvents = () => {
   return {
     type: RESET_CHARACTER_EVENTS,
   };
 };
-
+// Ação para buscar quadrinhos
 export const getComics = (character) => {
   const hash = hashGenerator();
   const URL = `https://gateway.marvel.com:443/v1/public/comics?titleStartsWith=${character}&ts=${TIME_STAMP}&apikey=${PUBLIC_KEY}&hash=${hash}`;
@@ -117,7 +116,7 @@ export const getComics = (character) => {
       .catch((err) => console.error(err));
   };
 };
-
+// Ação para buscar um quadrinho especifico
 export const getComic = (id) => {
   const hash = hashGenerator();
   const URL = `https://gateway.marvel.com:443/v1/public/comics/${id}?ts=${TIME_STAMP}&apikey=${PUBLIC_KEY}&hash=${hash}`;
@@ -131,7 +130,7 @@ export const getComic = (id) => {
       .catch((err) => console.error(err));
   };
 };
-
+// Ação para buscar eventos 
 export const getEvents = (eventName) => {
   const hash = hashGenerator();
   const URL = `https://gateway.marvel.com:443/v1/public/events?nameStartsWith=${eventName}&ts=${TIME_STAMP}&limit=10&apikey=${PUBLIC_KEY}&hash=${hash}`;
@@ -145,49 +144,35 @@ export const getEvents = (eventName) => {
       .catch((err) => console.error(err));
   };
 };
-
+// Ação para adicionar um personagem aos favoritos
 export const addCharacterToFavourites = (id) => {
   return {
     type: ADD_CHARACTER_TO_FAVOURITES,
     payload: id,
   };
 };
-
+// Remove um personagem dos favoritos
 export const removeCharacterFromFavourites = (id) => {
   return {
     type: REMOVE_CHARACTER_FROM_FAVOURITES,
     payload: id,
   };
 };
-
+//  adiciona um quadrinho aos favoritos
 export const addComicToFavourites = (id) => {
   return {
     type: ADD_COMIC_TO_FAVOURITES,
     payload: id,
   };
 };
-
+// remove um quadrinho dos favoritos
 export const removeComicFromFavourites = (id) => {
   return {
     type: REMOVE_COMIC_FROM_FAVOURITES,
     payload: id,
   };
 };
-
-// export const getSeries = () => {
-//   const hash = hashGenerator();
-//   const URL = `https://gateway.marvel.com:443/v1/public/series?creators=30&limit=50&ts=${TIME_STAMP}&apikey=${PUBLIC_KEY}&hash=${hash}`;
-//   return (dispatch) => {
-//     fetch(URL)
-//       .then((response) => response.json())
-//       .then((data) => {
-//         dispatch({ type: GET_SERIES, payload: data.data.results });
-//         console.log(data.data.results);
-//       })
-//       .catch((err) => console.error(err));
-//   };
-// };
-
+// Ação para buscar histórias
 export const getStories = () => {
   const hash = hashGenerator();
   const URL = `https://gateway.marvel.com:443/v1/public/stories?creators=30&limit=10&ts=${TIME_STAMP}&apikey=${PUBLIC_KEY}&hash=${hash}`;
@@ -201,22 +186,3 @@ export const getStories = () => {
       .catch((err) => console.error(err));
   };
 };
-
-// export const getCreators = (creator) => {
-//   const PUBLIC_KEY = "90f3c7ccb35efbb76e4bc98668685d38";
-//   const PRIVATE_KEY = "fd9435ff14b92b7935b7cb39495860414f142707";
-//   const TIME_STAMP = Date.now().toString();
-//   const HASH = md5.create();
-//   HASH.update(TIME_STAMP + PRIVATE_KEY + PUBLIC_KEY);
-//   const URL = `https://gateway.marvel.com:443/v1/public/creators?nameStartsWith=${creator}&ts=${TIME_STAMP}&apikey=${PUBLIC_KEY}&hash=${HASH.hex()}
-//   `;
-//   return (dispatch) => {
-//     fetch(URL)
-//       .then((response) => response.json())
-//       .then((data) => {
-//         dispatch({ type: GET_CREATORS, payload: data.data.results });
-//         console.log(data.data.results);
-//       })
-//       .catch((err) => console.error(err));
-//   };
-// };
